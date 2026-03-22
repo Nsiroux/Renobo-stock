de6bce4d2a1ad949eb41ba7b34efbcb18159fa42
Nicolas Siroux <nicolassiroux@Mac.localdomain>
2026-03-22 10:33:19 +0100

    temp fix for vercel build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // ⚠️ tijdelijk: laat build doorgaan ondanks TypeScript fouten
     ignoreBuildErrors: true,
   },
 }
